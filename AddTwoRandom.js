412761fca3e4d2216d3287c3cd300535c2760c81
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-09-06 19:17:20 +0530

    Added : addTwoNum

---
+++ AddTwoRandom.js
@@ -0,0 +1,6 @@
+let random = Math.floor(Math.random()*10);
+console.log(random);
+let random2 = Math.floor(Math.random()*10);
+console.log(random2);
+let result = random + random2;
+console.log (result);
